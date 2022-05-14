6fbb7865a0fe08b256c64ac055d57fdd9f387b81
Callum Quin <calllum.quin@gmail.com>
2022-05-14 18:31:08 +1200

    basename

--- src/index.js
+++ src/index.js
@@ -8,25 +8,29 @@ import { BrowserRouter } from "react-router-dom";
 import { createBreakpoints } from '@chakra-ui/theme-tools'
 
 const breakpoints = createBreakpoints({
-  sm: '30em',
-  md: '48em',
-  lg: '62em',
-  xl: '1300px',
-  '2xl': '96em'
+    sm: '30em',
+    md: '48em',
+    lg: '62em',
+    xl: '1300px',
+    '2xl': '96em'
 })
 
 const theme = extendTheme({ breakpoints })
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <BrowserRouter basename="/ev-punycode-generator">
-        <App />
-      </BrowserRouter>
-    </ChakraProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render( <
+    React.StrictMode >
+    <
+    ChakraProvider theme = { theme } >
+    <
+    BrowserRouter basename = "/ev-namecoinid-generator" >
+    <
+    App / >
+    <
+    /BrowserRouter> <
+    /ChakraProvider> <
+    /React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
